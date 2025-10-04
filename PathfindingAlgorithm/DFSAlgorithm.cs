3a5059c9cfee240f7fcd03bcbb49a0b54419e326
Leekiahn <68183471+Leekiahn@users.noreply.github.com>
2025-10-05 08:44:48 +0900

    feat : DFS 깊이 우선 탐색 구현

---
+++ PathfindingAlgorithm/DFSAlgorithm.cs
@@ -0,0 +1,29 @@
+﻿namespace PathfindingAlgorithm;
+
+public class DFSAlgorithm
+{
+    private readonly Graph graph;
+    private readonly HashSet<int> visited; // 방문한 노드 집합
+    
+    public DFSAlgorithm(Graph graph)
+    {
+        this.graph = graph;
+        this.visited = new HashSet<int>(); // visited 초기화
+    }
+
+    public void DFS(int start)
+    {
+        if(visited.Contains(start)) return;
+        
+        visited.Add(start);
+        Console.WriteLine($"Visited Node: {start}");
+
+        if (graph.AdjacencyList.ContainsKey(start))
+        {
+            foreach (var neighbor in graph.AdjacencyList[start])
+            {
+                DFS(neighbor);
+            }
+        }
+    }
+}
