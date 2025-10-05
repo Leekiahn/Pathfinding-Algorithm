46052d0f7141c558d9f4eea62a61a37cdd5916c9
Leekiahn <68183471+Leekiahn@users.noreply.github.com>
2025-10-05 09:21:01 +0900

    feat : BFS 너비 우선 탐색 구현

---
+++ PathfindingAlgorithm/BFSAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿namespace PathfindingAlgorithm;
+
+public class BFSAlgorithm
+{
+    private readonly Graph graph;
+    
+    public BFSAlgorithm(Graph graph)
+    {
+        this.graph = graph;
+    }
+    
+    // 큐 기반 BFS 구현
+    public void BFS(int start)
+    {
+        var visited = new bool[graph.AdjacencyList.Count];
+        var queue = new Queue<int>();
+        queue.Enqueue(start);
+
+        while (queue.Count > 0)
+        {
+            int node = queue.Dequeue();
+            if (visited[node]) continue;    // 이미 방문한 노드면 무시
+            
+            visited[node] = true;   // 방문 처리
+            Console.WriteLine($"Visited Node: {node}");
+
+            // 인접 노드 큐에 추가
+            if (graph.AdjacencyList.ContainsKey(node))
+            {
+                foreach (var neighbor in graph.AdjacencyList[node])
+                {
+                    if (!visited[neighbor])
+                    {
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+        }
+    }
+}
