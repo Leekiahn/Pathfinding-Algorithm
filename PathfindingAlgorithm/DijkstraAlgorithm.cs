5b1d1285dd22789e86f5902c89c2d2b2e497b9f2
Leekiahn <68183471+Leekiahn@users.noreply.github.com>
2025-10-08 17:04:09 +0900

    refactor: 힙 기반 다익스트라 알고리즘으로 수정

--- PathfindingAlgorithm/DijkstraAlgorithm.cs
+++ PathfindingAlgorithm/DijkstraAlgorithm.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace PathfindingAlgorithm;
+﻿namespace PathfindingAlgorithm;
 
 public class DijkstraAlgorithm
 {
@@ -15,9 +12,9 @@ public class DijkstraAlgorithm
     // 다익스트라 알고리즘 구현
     public (int[] distances, int[] previous) Dijkstra(int start)
     {
-        int[] distances = new int[graph.size]; // 시작 노드로부터 모든 정점까지의 최단 거리
-        int[] previous = new int[graph.size];  // 최단 경로 추적용 이전 노드
-        SortedSet<(int distance, int node)> priorityQueue = new SortedSet<(int distance, int node)>(); // 우선순위 큐
+        int[] distances = new int[graph.size]; // 최단 거리 배열
+        int[] previous = new int[graph.size];  // 이전 노드 배열
+        PriorityQueue<int, int> priorityQueue = new PriorityQueue<int, int>(); // 우선순위 큐 
 
         for (int i = 0; i < graph.size; i++)
         {
@@ -26,27 +23,26 @@ public class DijkstraAlgorithm
         }
 
         distances[start] = 0;   // 시작 노드까지의 거리는 0
-        priorityQueue.Add((0, start));  // 시작 노드 추가
+        priorityQueue.Enqueue(0, start);  // 시작 노드 추가
         
         while (priorityQueue.Count > 0)
         {
-            int currentDistance = priorityQueue.Min.distance;
-            int currentNode = priorityQueue.Min.node;
-            priorityQueue.Remove(priorityQueue.Min);
-
+            priorityQueue.TryDequeue(out int currentDistance, out int currentNode);   // 선택한 노드 제거
+            
             if (currentDistance > distances[currentNode])
             {
                 continue; // 이미 더 짧은 경로를 찾았으면 무시
             }
-
+            
+            // 인접 노드 탐색
             foreach ((int neighbor, int weight) in graph.adjList[currentNode])
             {
-                int distance = currentDistance + weight;
-                if (distance < distances[neighbor])
+                int distance = currentDistance + weight;    // 현재 노드를 거쳐가는 거리 계산
+                if (distance < distances[neighbor])     // 더 짧은 경로 발견 시 업데이트
                 {
-                    distances[neighbor] = distance;
-                    previous[neighbor] = currentNode;
-                    priorityQueue.Add((distance, neighbor));
+                    distances[neighbor] = distance; // 최단 거리 갱신
+                    previous[neighbor] = currentNode;   // 이전 노드 갱신
+                    priorityQueue.Enqueue(distance, neighbor);    // 우선순위 큐에 추가
                 }
             }
         }
--- PathfindingAlgorithm/Program.cs
+++ PathfindingAlgorithm/Program.cs
@@ -4,41 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Graph graph = new Graph(7, false);
-        graph.AddEdge(0, 1);
-        graph.AddEdge(0, 2);
-        graph.AddEdge(1, 3);
-        graph.AddEdge(2, 3);
-        graph.AddEdge(3, 4);
-        graph.AddEdge(3, 5);
-        graph.AddEdge(4, 6);
-        graph.AddEdge(5, 6);
-
-        DFSAlgorithm dfs = new DFSAlgorithm(graph);
-        Console.WriteLine("깊이 우선 탐색");
-        dfs.DFS(0);
-        
-        
-        Console.WriteLine("---------------------");
-        BFSAlgorithm bfs = new BFSAlgorithm(graph);
-        Console.WriteLine("너비 우선 탐색");
-        bfs.BFS(0);
-
-        
-        WeightedGraph weightedGraph = new WeightedGraph(6, false);
+        WeightedGraph weightedGraph = new WeightedGraph(5, false);
         weightedGraph.AddEdge(0, 1, 3);
         weightedGraph.AddEdge(0, 2, 4);
-        weightedGraph.AddEdge(1, 2, 5);
-        weightedGraph.AddEdge(1, 3, 5);
-        weightedGraph.AddEdge(1, 4, 7);
+        weightedGraph.AddEdge(1, 3, 10);
+        weightedGraph.AddEdge(1, 4, 5);
+        weightedGraph.AddEdge(2, 3, 5);
         weightedGraph.AddEdge(2, 4, 2);
-        weightedGraph.AddEdge(2, 5, 1);
-        weightedGraph.AddEdge(3, 4, 2);
-        weightedGraph.AddEdge(4, 5, 4);
+        weightedGraph.AddEdge(3, 4, 3);
         weightedGraph.PrintList();
         
         DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(weightedGraph);
-        Console.WriteLine("다익스트라 알고리즘");
         var result = dijkstra.Dijkstra(0);
         for (int i = 0; i < result.distances.Length; i++)
         {
